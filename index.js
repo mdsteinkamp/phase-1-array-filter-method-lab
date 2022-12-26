214f96275096461504f0f2d1906b8c274f97b5f4
Matthew Steinkamp <mdsteinkamp@gmail.com>
2022-12-26 11:17:42 -0800

    commit tree a18dae9e859978985a6ebe83637d5d65014a2cfe

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(array, enteredName) {
+  return array.filter(name => enteredName.toUpperCase().includes(name.toUpperCase()));
+};
+
+function fuzzyMatch(array, enteredName) {
+  return array.filter(name => enteredName.slice(0, 2).includes(name.slice(0,2)));
+};
+
+
+const driversObj = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+];
+
+//below is for an array of objects 
+function matchName(array, enteredName) {
+
+    return array.filter(array => array.name.toUpperCase() === enteredName.toUpperCase())
+}
